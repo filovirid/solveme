839b05e597930487c1de324621f97a1d20fb82c7
filovirid <filovirid@aicit.org>
2020-01-29 09:36:24 +0100

    add asm01 question

---
+++ asmex/asm01/asm_01.asm
@@ -0,0 +1,21 @@
+01: 8B 7D 08    mov edi, [ebp+8]
+
+02: 8B D7       mov edx, edi
+
+03: 33 C0       xor eax, eax
+
+04: 83 C9 FF    or ecx, 0FFFFFFFFh
+
+05: F2 AE       repne scasb
+
+06: 83 C1 02    add ecx, 2
+
+07: F7 D9       neg ecx
+
+08: 8A 45 0C    mov al, [ebp+0Ch]
+
+09: 8B FA       mov edi, edx
+
+10: F3 AA       rep stosb
+
+11: 8B C2       mov eax, edx
